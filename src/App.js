a2ec11d06bb39cfc4ebbae1138f37478c4d76ebb
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-18 19:11:22 +0100

    add NavLinks and view components (Rockets, missions,my profile)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import './styles/App.css';
+import Router from './routing/Router';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Router />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,38 @@
-const NavBar = () => (
-  <>
-    <header>
-      <nav className="nav-container">
-        <img src="../images/saturn.png" alt="" className="image-logo" />
-        <h2>Space Traveler&#39;s Hub</h2>
-      </nav>
-    </header>
-  </>
-);
+/* eslint-disable */
+import { NavLink } from 'react-router-dom';
+import NavLinkEl from './NavLinkEl';
 
+const NavBar = () => {
+  const activeStyle = {
+    textDecoration: 'underline',
+  };
+
+  return (
+    <>
+      <header>
+        <nav className="nav-container">
+          <div className="logo-wrapper">
+            <img src="../images/saturn.png" alt="" className="image-logo" />
+            <h2>Space Traveler&#39;s Hub</h2>
+          </div>
+          <div>
+            <NavLinkEl
+              path={"/"}
+              name="Rockets"
+            />
+            <NavLinkEl
+              path={"missions"}
+              name="Missions"
+            />
+            { "|" }
+            <NavLinkEl
+              path={"profile"}
+              name="My Profile"
+            />            
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
 export default NavBar;
