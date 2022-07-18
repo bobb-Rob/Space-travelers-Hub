5c91271570dae7982f5aea007d42782bfdcb23d1
bigmosi <kinyeramo@gmail.com>
2022-07-18 18:49:39 +0300

    added nav logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,12 @@
-const NavBar = () => <header><nav> </nav></header>;
+const NavBar = () => (
+  <>
+    <header>
+      <nav className="nav-container">
+        <img src="../images/saturn.png" alt="" className="image-logo" />
+        <h2>Space Traveler&#39;s Hub</h2>
+      </nav>
+    </header>
+  </>
+);
 
 export default NavBar;
