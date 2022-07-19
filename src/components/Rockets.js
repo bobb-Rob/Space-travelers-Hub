47b1b6a4e87a740e260bfda7e48681297f0e26dc
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-19 22:49:51 +0100

    display rockets properties

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,11 +1,27 @@
 import { useSelector } from 'react-redux';
+import RocketCard from './RocketCard';
 import '../styles/rockets.css';
 
 const Rockets = () => {
   const state = useSelector((state) => state.rocketReducer);
   console.log(state);
+
   return (
-    <div className="rockets-list-container">Rockets</div>
+    <div className="rockets-list-container">
+      {state.map((rocket) => {
+        const {
+          description, rocketName, flickrImage, id,
+        } = rocket;
+        return (
+          <RocketCard
+            key={id}
+            description={description}
+            image={flickrImage}
+            name={rocketName}
+          />
+        );
+      })}
+    </div>
   );
 };
 export default Rockets;
