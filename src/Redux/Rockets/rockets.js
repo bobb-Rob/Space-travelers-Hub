e735d458b6a39fdfd17dd403dbb3c1ea8491c963
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-19 18:46:24 +0100

    Add a helper function "getSelectField" to select required field from the fetch return object

--- src/Redux/Rockets/rockets.js
+++ src/Redux/Rockets/rockets.js
@@ -1,11 +1,25 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+const getSelectField = (rockets) => {
+  let newRockets = [];
+  rockets.forEach((rocket) => {
+    const selectedField = {};
+    selectedField.id = rocket.id;
+    selectedField.name = rocket.rocket_name;
+    selectedField.type = rocket.rocket_type;
+    const [imageOne] = rocket.flickr_images;
+    selectedField.flickr_images = imageOne;
+    newRockets = [...newRockets, selectedField];
+  });
+  return newRockets;
+};
+
 export const getRockets = createAsyncThunk(
   'rockets/getRockets',
   async () => {
     const response = await fetch('https://api.spacexdata.com/v3/rockets');
     const rockets = await response.json();
-    return rockets;
+    return getSelectField(rockets);
   },
 );
 
