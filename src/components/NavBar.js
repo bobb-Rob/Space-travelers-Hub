c825b1d3449e0bf1bc9b0b100a8a5d4c61137a39
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-21 08:01:52 +0100

    remove unnece

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,31 @@
-/* eslint-disable */
-import { NavLink } from 'react-router-dom';
 import NavLinkEl from './NavLinkEl';
 
-const NavBar = () => {
-  const activeStyle = {
-    textDecoration: 'underline',
-  };
+const NavBar = () => (
+  <>
+    <header>
+      <nav className="nav-container">
+        <div className="logo-wrapper">
+          <img src="../images/saturn.png" alt="" className="image-logo" />
+          <h2>Space Traveler&#39;s Hub</h2>
+        </div>
+        <div>
+          <NavLinkEl
+            path="/"
+            name="Rockets"
+          />
+          <NavLinkEl
+            path="missions"
+            name="Missions"
+          />
+          |
+          <NavLinkEl
+            path="profile"
+            name="My Profile"
+          />
+        </div>
+      </nav>
+    </header>
+  </>
+);
 
-  const handleClick = () => {
-    console.log('I can dispatch fetch from here');
-  }
-
-  return (
-    <>
-      <header>
-        <nav className="nav-container">
-          <div className="logo-wrapper">
-            <img src="../images/saturn.png" alt="" className="image-logo" />
-            <h2>Space Traveler&#39;s Hub</h2>
-          </div>
-          <div>
-            <NavLinkEl
-              path={"/"}
-              name="Rockets"
-            />
-            <NavLinkEl
-              path={"missions"}
-              name="Missions"
-              handleClick={handleClick}
-            />
-            { "|" }
-            <NavLinkEl
-              path={"profile"}
-              name="My Profile"
-            />            
-          </div>
-        </nav>
-      </header>
-    </>
-  );
-};
 export default NavBar;
