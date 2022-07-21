c2ed40abb812d118199313db7c2931f12e9269e0
bigmosi <kinyeramo@gmail.com>
2022-07-21 14:14:02 +0300

    worked on my mission profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,7 +1,30 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import '../styles/myProfile.css';
+import { getMission } from '../Redux/mission';
 
-const MyProfile = () => (
-  <div className="my-profile-container">MyProfile</div>
-);
+const MyProfile = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missionReducer);
 
+  useEffect(() => {
+    if (missions.length === 0) {
+      dispatch(getMission());
+    }
+  }, []);
+  return (
+    <>
+      <div className="my-profile-container">
+        <h2>My Mission</h2>
+        <ul className="list-unstyled">
+          {missions.filter((mission) => mission.reserved === true).map((fmission) => (
+            <li key={fmission.id} className="border p-3 w-50">
+              {fmission.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
 export default MyProfile;
